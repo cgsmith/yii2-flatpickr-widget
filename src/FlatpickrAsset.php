990a44cfa8932d71170c2714e1a7ef99f9a1801b
Dmitry Polozov <pdapnz@yandex.ru>
2024-09-25 09:55:27 +0300

    third party CDN replaced with npm-asset/flatpickr

--- src/FlatpickrAsset.php
+++ src/FlatpickrAsset.php
@@ -6,15 +6,19 @@ use yii\web\JqueryAsset;
 
 class FlatpickrAsset extends \yii\web\AssetBundle
 {
+    public $sourcePath = '@npm/flatpickr';
+
     public $js = [
-        '//cdn.jsdelivr.net/npm/flatpickr',
+        'dist/flatpickr.min.js',
     ];
 
     public $css = [
-        '//cdn.jsdelivr.net/npm/flatpickr/dist/flatpickr.min.css',
+        'dist/flatpickr.min.css',
     ];
 
     public $depends = [
         JqueryAsset::class,
     ];
+
+
 }
